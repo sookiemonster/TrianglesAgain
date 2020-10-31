c68d59f24e7c9697ea7e829290fb208b248b6fc8
sookiemonster <dsooknanan20@stuy.edu>
2020-10-31 18:00:49 -0400

    Created Triangle.java, added triangle constructors & tests for the constructors.

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 public class Tester {
   public static void main(String args[]) {
     Point A = new Point(-1.0, -2.0);
-    Point B = new Point(A);
+    Point B = new Point(1.0, 3.0);
     Point C = new Point(3.0, 4.0);
 
     corToPoint( A.getX(), A.getY() );
@@ -12,6 +12,9 @@ public class Tester {
 
     System.out.println(A.equals(B));
     System.out.println(B.equals(C));
+
+    Triangle ABC = new Triangle(A, B, C);
+    Triangle DEF = new Triangle(1.0, 2.0, 2.5, 4.0, 3.0, 3.0);
     }
 
   public static void corToPoint(double x, double y) {
